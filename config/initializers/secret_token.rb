00a04a8839351927c491dd08cbb707f067727adf
Mike Nasser <bignass04@gmail.com>
2014-06-15 23:24:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fashionservices::Application.config.secret_key_base = '093b8b610600a88aff8ce06a7ffb5b4710f74f376fa4c060f8d6894ac17578bffaa07af5a32afddfc5d1f37cef1130ff313ce6834896a1c1176ed63a3ab908ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fashionservices::Application.config.session_store :cookie_store, key: '_fashionservices_session'
